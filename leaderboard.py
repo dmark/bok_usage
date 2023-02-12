adc1ad917ef958ce26886e5fe3c76c6e77b88a1e
George Dobbs <george.dobbs@gmail.com>
2023-02-12 13:38:21 -0500

    top 20 in graphic

--- leaderboard.py
+++ leaderboard.py
@@ -1,22 +1,21 @@
+#! /usr/bin/env python
 '''grab the to n all time countries'''
 import logging
 
 import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
-import numpy as np
 
 from time_series import read_data
-
+out_file='data/leader_usage.png'
 logging.basicConfig(level=logging.INFO)
 logger=logging.getLogger(__name__ )
-n=30
+N=20
 
 lb_file='data/leader_board.xlsx'
 df=read_data()
 logger.info ('Number of rows is: %d'% df.shape[0])
 df['Total']=df.sum(axis=1)
 df.sort_values(by='Total',ascending=False,inplace=True)
-print(df.head(n))
+print(df.head(N))
 
 df.to_excel(lb_file)
 logger.info('Wrote ' + lb_file)
@@ -30,17 +29,17 @@ month=['%d-%02d'%(divmod(m,100)) for m in month]
 df.columns=month
 outlier=False
 if outlier:
-  for ix,row in df.head(1).iterrows():
+  for ix,row in df.head(N).iterrows():
     vals=row.tolist()
     country='United States'
     if ix==country: # hack to adjust outlier
       x=vals.index(8645)
       df.at[country,month[x]]=2000
 
-ax=df.head(12).T.plot.bar(rot=70, stacked=True)
+ax=df.head(N).T.plot.bar(rot=70, stacked=True)
 ax.figure.set_size_inches(10,7)
 ax.set_title('BoK Usage')
 ax.set_xlabel('Month')
 ax.set_ylabel('Number of downloads')
-plt.savefig('data/leader_usage.png')
-#plt.show()
+plt.savefig(out_file)
+logger.info('Leader graphic saved to %s'%(out_file))
